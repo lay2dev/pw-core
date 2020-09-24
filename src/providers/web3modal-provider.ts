bed968480ad0df024cce2bc38586e8edcfd6258e
johnz <johnz@lay2.dev>
2020-09-24 22:42:21 +0800

    change web3modal-provider api parameters

--- src/providers/web3modal-provider.ts
+++ src/providers/web3modal-provider.ts
@@ -1,16 +1,14 @@
 import { Provider, Platform } from './provider';
-import { Address, AddressType } from '..';
+import { Address, AddressType } from '../models';
 import ENS from 'ethereum-ens';
-import Web3 from 'web3';
+// import Web3 from 'web3';
 import { verifyEthAddress } from '../utils';
 
 export class Web3ModalProvider extends Provider {
   onAddressChanged: (newAddress: Address) => void;
 
-  web3: Web3;
-
   constructor(
-    readonly web3Provider: any,
+    readonly web3: any,
     onAddressChanged?: (newAddress: Address) => void
   ) {
     super(Platform.eth);
@@ -18,18 +16,20 @@ export class Web3ModalProvider extends Provider {
   }
 
   async init(): Promise<Provider> {
-    this.web3 = new Web3(this.web3Provider);
     const accounts = await this.web3.eth.getAccounts();
     if (!verifyEthAddress(accounts[0])) {
       throw new Error('get ethereum address failed');
     }
 
     this.address = new Address(accounts[0], AddressType.eth);
-    if (this.web3Provider.on) {
-      this.web3Provider.on('accountsChanged', async (newAccounts: string[]) => {
-        this.address = new Address(newAccounts[0], AddressType.eth);
-        this.onAddressChanged(this.address);
-      });
+    if (this.web3.currentProvider.on) {
+      this.web3.currentProvider.on(
+        'accountsChanged',
+        async (newAccounts: string[]) => {
+          this.address = new Address(newAccounts[0], AddressType.eth);
+          this.onAddressChanged(this.address);
+        }
+      );
     }
 
     return this;
@@ -37,7 +37,7 @@ export class Web3ModalProvider extends Provider {
 
   async ensResolver(ens: string): Promise<string> {
     try {
-      return await new ENS(this.web3Provider).resolver(ens).addr();
+      return await new ENS(this.web3.currentProvider).resolver(ens).addr();
     } catch (e) {
       return 'Unknown ENS Name';
     }
@@ -58,8 +58,12 @@ export class Web3ModalProvider extends Provider {
   }
 
   async close() {
-    if (this.web3 && this.web3Provider && this.web3Provider.close) {
-      await this.web3Provider.close();
+    if (
+      this.web3 &&
+      this.web3.currentProvider &&
+      this.web3.currentProvider.close
+    ) {
+      await this.web3.currentProvider.close();
     }
   }
 }
