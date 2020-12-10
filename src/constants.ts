0c81e179c39a17b8d0ef2bdcb1e6af1612063102
zhixian <frank@lay2.dev>
2020-12-10 18:03:33 +0800

    feat(config): update sudt config on aggron

--- src/constants.ts
+++ src/constants.ts
@@ -129,12 +129,12 @@ export const CHAIN_SPECS = {
       cellDep: new CellDep(
         DepType.code,
         new OutPoint(
-          '0xc1b2ae129fad7465aaa9acc9785f842ba3e6e8b8051d899defa89f5508a77958',
+          '0xe12877ebd2c3c364dc46c5c992bcfaf4fee33fa13eebdf82c591fc9825aab769',
           '0x0'
         )
       ),
       script: new Script(
-        '0x48dbf59b4c7ee1547238021b4869bceedf4eea6b43772e5d66ef8865b6ae7212',
+        '0xc5e5dcf215925f7ef4dfaf5f4b4f105bc321c02776d6e7d52a1db3fcd9d011a4',
         '0x',
         HashType.data
       ),
