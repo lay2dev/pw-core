1f5f1e424d134de5d38457ec88b5bc6969f88796
johnz <johnz@lay2.dev>
2021-05-14 21:47:40 +0800

    fix(eth-provider): update ethereum connector for metamask depcrated ethereum.enable()

--- src/providers/eth-provider.ts
+++ src/providers/eth-provider.ts
@@ -11,7 +11,10 @@ export class EthProvider extends Provider {
   async init(): Promise<Provider> {
     if (typeof window.ethereum !== 'undefined') {
       window.ethereum.autoRefreshOnNetworkChange = false;
-      const accounts = await window.ethereum.enable();
+      // const accounts = await window.ethereum.enable();
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
       this.address = new Address(accounts[0], AddressType.eth);
 
       if (!!window.ethereum.on) {
@@ -55,30 +58,44 @@ export class EthProvider extends Provider {
   async sign(message: string): Promise<string> {
     return new Promise((resolve, reject) => {
       const from = this.address.addressString;
-      const params = [message, from];
-      const method = 'personal_sign';
 
-      window.web3.currentProvider.sendAsync(
-        { method, params, from },
-        (err, result) => {
-          if (err) {
-            reject(err);
-          }
-          if (result.error) {
-            reject(result.error);
-          }
-          result = result.result;
-          let v = Number.parseInt(result.slice(-2), 16);
-          if (v >= 27) v -= 27;
-          result =
-            '0x' +
-            this.platform.toString(16).padStart(2, '0') +
-            result.slice(2, -2) +
-            v.toString(16).padStart(2, '0');
+      const handleResult = (result): string => {
+        let v = Number.parseInt(result.slice(-2), 16);
+        if (v >= 27) v -= 27;
+        result =
+          '0x' +
+          this.platform.toString(16).padStart(2, '0') +
+          result.slice(2, -2) +
+          v.toString(16).padStart(2, '0');
+        return result;
+      };
 
-          resolve(result);
-        }
-      );
+      if (typeof window.ethereum !== 'undefined') {
+        window.ethereum
+          .request({ method: 'personal_sign', params: [from, message] })
+          .then((result) => {
+            resolve(handleResult(result));
+          });
+      } else if (!!window.web3) {
+        window.web3.currentProvider.sendAsync(
+          { method: 'personal_sign', params: [message, from], from },
+          (err, result) => {
+            if (err) {
+              reject(err);
+            }
+            if (result.error) {
+              reject(result.error);
+            }
+            resolve(handleResult(result.result));
+          }
+        );
+      } else {
+        reject(
+          new Error(
+            'window.ethereum/window.web3 is undefined, Ethereum environment is required.'
+          )
+        );
+      }
     });
   }
 
