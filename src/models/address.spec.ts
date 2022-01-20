113e7dfa2d0f63e623c4e29eb23ab99e58d1d500
Daniel Kmak <neverkuzi@gmail.com>
2022-01-20 16:44:27 +0100

    feat: CKB2021 compatibility

--- src/models/address.spec.ts
+++ src/models/address.spec.ts
@@ -26,7 +26,7 @@ const tronAddress = new Address(tron, AddressType.tron);
 const network = {
   blockchain: 'eos',
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-  host: 'eospush.tokenpocket.pro',
+  host: 'mainnet.eosamsterdam.net',
   port: 80,
   protocol: 'http',
 };
